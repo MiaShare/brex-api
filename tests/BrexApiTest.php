9710860234a40b32a62cba215d5e1bbe8a276ed1
hotsaucejake <get@hotsaucejake.com>
2022-02-23 15:30:40 -0500

    initial setup

---
+++ tests/BrexApiTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BrexApiTest extends TestCase
+{
+    public function testInitialTestingWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
